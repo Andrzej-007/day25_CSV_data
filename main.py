4723e0b40b61fcb5a98ea0ab271b22300f2aa8f4
andrzej_kozlowski <andrzej_kozlowski@interia.eu>
2021-09-24 18:36:22 +0200

    first_start

--- main.py
+++ main.py
@@ -21,21 +21,27 @@
 import pandas as pd
 
 data = pd.read_csv("./weather_data.csv")
-print(type(data))
-print('\n')
-print(data)
-print(data['temp'])
+# print(type(data))
+# print('\n')
+# print(data)
+# print(data['temp'])  # there is the same structure as below
+# print(data.temp)    # there is the same structure as above
+#
+# dataDict = data.to_dict()  # transfert to dictionaty
+# print(dataDict)
+#
+# list_temp = data['temp'].to_list()
+# print('\n',list_temp )
+#
+# average  = sum(list_temp)/len(list_temp)
+# print(round(average, 2))
+#
+# # Series
+#
+# print(data['temp'].mean())
+# print(data['temp'].max())
 
-dataDict = data.to_dict()  # transfert to dictionaty
-print(dataDict)
-
-list_temp = data['temp'].to_list()
-print('\n',list_temp )
-
-average  = sum(list_temp)/len(list_temp)
-print(round(average, 2))
-
-# Series
-
-print(data['temp'].mean())
-print(data['temp'].max())
+# Get Data in Raw
+temp_max = data.temp.max()
+print(data[data.day == 'Monday'])
+print(data[data.temp == data.temp.max()])
